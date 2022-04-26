01258eccb677c4738c165a61a5a3d200f089c6aa
Kristanto Sean <kristantosean@gmail.com>
2022-04-26 15:44:04 +0700

    Initial Commit

---
+++ BMI Calculator SwiftUI/BMI_Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_Calculator_SwiftUIApp.swift
+//  BMI Calculator SwiftUI
+//
+//  Created by Kristanto Sean N on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BMI_Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
